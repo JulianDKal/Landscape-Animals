19398e3ea233a2f075d218870f6a6d1e457c6b89
JulianDKal <juliandk1357@gmail.com>
2022-06-16 01:25:49 +0200

    hexagon grid algorithm + various improvements

---
+++ Assets/Scripts/Hex.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+class Hex
+{
+
+    public Hex(int q, int r, int s)
+    {
+        if (q + r + s == 0)
+        {
+            this.q = q;
+            this.r = r;
+            this.s = s;
+        }
+        else Debug.LogWarning("q + r +s does not equal zero!");
+    }
+
+    public int q;
+    public int r;
+    public int s;
+    }
+
